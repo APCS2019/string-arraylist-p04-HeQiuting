24ae32494e6a9b10b8282e153d7f3e330e19863a
HeQiuting <45375773+HeQiuting@users.noreply.github.com>
2018-12-09 22:45:06 +0800

    Create 20181209-Maggie.java

---
+++ 20181209-Maggie.java
@@ -0,0 +1,11 @@
+(a)
+public String decodeString(ArrayList<StringParts> parts){
+  String tmp = "";
+  for(int i = 0; i < parts.size(); i++){
+    tmp = parts.get(i);
+  }
+  return tmp;
+  
+(b)
+public ArrayList<StringPart> encodeString(String word)
+    
